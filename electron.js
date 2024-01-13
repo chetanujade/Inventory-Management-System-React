75ffcc02a96e8c8b0d3fba53f92e4285d6a08532
chetanujade <ujadechetan@gmail.com>
2024-01-13 14:36:00 +0530

    Updates

---
+++ electron.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+function createWindow() {
+    const { width, height } = require('electron').screen.getPrimaryDisplay().workAreaSize;
+    const startUrl = process.env.ELECTRON_START_URL || url.format({
+        pathname: path.join(__dirname, '../index.html'),
+        protocol: 'file:',
+        slashes: true,
+    });
+    const win = new BrowserWindow({
+        width: width,
+        height: height,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    win.webContents.setWindowOpenHandler((details) => {
+        shell.openExternal(details.url); // Open URL in user's browser.
+        return { action: "deny" };
+    });
+    win.loadURL(startUrl);
+    app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') {
+            app.quit()
+        }
+    });
+}
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+});
